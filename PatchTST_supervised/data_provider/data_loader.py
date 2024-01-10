dd29f5a0ba7d33f0fc4d2c9c58fd774882fddcb3
Zhijun Pan <ljacksonpan@outlook.com>
2024-01-10 14:30:56 +0000

    edited for atm

--- PatchTST_supervised/data_provider/data_loader.py
+++ PatchTST_supervised/data_provider/data_loader.py
@@ -227,9 +227,9 @@ class Dataset_Custom(Dataset):
         df_raw.columns: ['date', ...(other features), target feature]
         '''
         cols = list(df_raw.columns)
-        cols.remove(self.target)
-        cols.remove('date')
-        df_raw = df_raw[['date'] + cols + [self.target]]
+        # cols.remove(self.target)
+        # cols.remove('date')
+        # df_raw = df_raw[['date'] + cols + [self.target]]
         # print(cols)
         num_train = int(len(df_raw) * 0.7)
         num_test = int(len(df_raw) * 0.2)
@@ -254,21 +254,22 @@ class Dataset_Custom(Dataset):
         else:
             data = df_data.values
 
-        df_stamp = df_raw[['date']][border1:border2]
-        df_stamp['date'] = pd.to_datetime(df_stamp.date)
-        if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            data_stamp = df_stamp.drop(['date'], axis=1).values
-        elif self.timeenc == 1:
-            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
-            data_stamp = data_stamp.transpose(1, 0)
+        # df_stamp = df_raw[['date']][border1:border2]
+        # df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        # if self.timeenc == 0:
+        #     df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+        #     df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+        #     df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+        #     df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+        #     data_stamp = df_stamp.drop(['date'], axis=1).values
+        # elif self.timeenc == 1:
+        #     data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+        #     data_stamp = data_stamp.transpose(1, 0)
 
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
-        self.data_stamp = data_stamp
+        # self.data_stamp = data_stamp
+        self.data_stamp = df_raw[['DeltaTime']].values[border1:border2]
 
     def __getitem__(self, index):
         s_begin = index
